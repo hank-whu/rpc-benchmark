388bf7d2a978cbe828f97c4da271c896c8088d44
Hank <hankai@coolchuan.com>
2018-01-27 17:58:28 +0800

    完善 tars

--- tars-client/src/main/java/benchmark/rpc/tars/UserServiceTarsClientImpl.java
+++ tars-client/src/main/java/benchmark/rpc/tars/UserServiceTarsClientImpl.java
@@ -1,7 +1,12 @@
 package benchmark.rpc.tars;
 
+import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +23,53 @@ import benchmark.service.UserService;
 
 public class UserServiceTarsClientImpl implements UserService, Closeable {
 
-	private CommunicatorConfig cfg = new CommunicatorConfig();
+	private CommunicatorConfig cfg;
 	// 构建通信器
-	private Communicator communicator = CommunicatorFactory.getInstance().getCommunicator(cfg);
+	private Communicator communicator;
 	// 通过通信器，生成代理对象
-	private TarsUserServicePrx proxy = communicator.stringToProxy(TarsUserServicePrx.class,
-			"TestApp.HelloServer.UserService@tcp -h benchmark-server -p 18600 -t 60000");
+	private TarsUserServicePrx proxy;
+
+	public UserServiceTarsClientImpl() {
+		new File("/tmp/data").mkdirs();
+		new File("/tmp/log").mkdirs();
+
+		try {
+			File tarsConf = copyToTemp("/tars.conf");
+
+			System.setProperty("config", tarsConf.getAbsolutePath());
+			System.out.println(System.getProperty("config"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		cfg = new CommunicatorConfig();
+		cfg.setLogPath("/tmp/log");
+		cfg.setDataPath("/tmp/data");
+
+		communicator = CommunicatorFactory.getInstance().getCommunicator(cfg);
+
+		proxy = communicator.stringToProxy(TarsUserServicePrx.class,
+				"TestApp.HelloServer.UserService@tcp -h benchmark-server -p 18600 -t 60000");
+	}
+
+	private static File copyToTemp(String name) throws Exception {
+		File file = new File("/tmp/" + name);
+
+		try (FileOutputStream fos = new FileOutputStream(file);
+				InputStream is = UserServiceTarsClientImpl.class.getResourceAsStream(name);
+				BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));) {
+
+			String line = "";
+
+			while ((line = br.readLine()) != null) {
+				fos.write(line.getBytes("UTF-8"));
+				fos.write("\r\n".getBytes("UTF-8"));
+			}
+
+		}
+
+		return file;
+	}
 
 	@Override
 	public void close() throws IOException {
--- tars-server/src/main/java/benchmark/rpc/Server.java
+++ tars-server/src/main/java/benchmark/rpc/Server.java
@@ -1,16 +1,44 @@
 package benchmark.rpc;
 
+import java.io.BufferedReader;
 import java.io.File;
-
-import com.qq.tars.server.startup.Main;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Server {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Exception {
 		new File("/tmp/data").mkdirs();
 		new File("/tmp/log").mkdirs();
 
-		Main.main(args);
+		File tarsConf = copyToTemp("/tars.conf");
+
+		System.setProperty("config", tarsConf.getAbsolutePath());
+		System.out.println(System.getProperty("config"));
+
+		new com.qq.tars.server.core.Server().startUp(args);
+
+		Thread.sleep(Long.MAX_VALUE);
+	}
+
+	private static File copyToTemp(String name) throws Exception {
+		File file = new File("/tmp/" + name);
+
+		try (FileOutputStream fos = new FileOutputStream(file);
+				InputStream is = Server.class.getResourceAsStream(name);
+				BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));) {
+
+			String line = "";
+
+			while ((line = br.readLine()) != null) {
+				fos.write(line.getBytes("UTF-8"));
+				fos.write("\r\n".getBytes("UTF-8"));
+			}
+
+		}
+
+		return file;
 	}
 
 }
