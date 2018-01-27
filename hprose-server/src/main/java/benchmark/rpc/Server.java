4d7a9117693bb7354408341ca9df139700b6de85
Hank <hankai@coolchuan.com>
2018-01-27 10:06:28 +0800

    修复 hprose 无法启动的问题

--- hprose-server/src/main/java/benchmark/rpc/Server.java
+++ hprose-server/src/main/java/benchmark/rpc/Server.java
@@ -6,13 +6,11 @@ import hprose.server.HproseTcpServer;
 public class Server {
 
 	public static void main(String[] args) throws Exception {
-		HproseTcpServer server = new HproseTcpServer("tcp://benchmark-server:8080");
+		HproseTcpServer server = new HproseTcpServer("benchmark-server", 8080);
 		server.add(new UserServiceServerImpl());
 		server.start();
 		System.out.println("START");
-		System.in.read();
-		server.stop();
-		System.out.println("STOP");
+		Thread.sleep(Long.MAX_VALUE);
 	}
 
 }
