3e588f5f9c50a757b463c269972b0088336783d0
Hank <hank.whu@gmail.com>
2018-05-03 18:28:29 +0800

    初步完成 servicecomb

--- servicecomb-client/src/main/java/benchmark/rpc/Client.java
+++ servicecomb-client/src/main/java/benchmark/rpc/Client.java
@@ -2,6 +2,9 @@ package benchmark.rpc;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
+import org.apache.servicecomb.provider.pojo.RpcReference;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
@@ -9,21 +12,36 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
+import org.springframework.stereotype.Component;
 
 import benchmark.bean.Page;
 import benchmark.bean.User;
 import benchmark.service.UserService;
-import benchmark.service.UserServiceJsonHttpClientImpl;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 @State(Scope.Benchmark)
+@Component
 public class Client extends AbstractClient {
 	public static final int CONCURRENCY = 32;
 
-	private final UserService userService = new UserServiceJsonHttpClientImpl(CONCURRENCY);
+	@RpcReference(microserviceName = "benchmark", schemaId = "benchmark")
+	private static UserService userService;
+
+	static {
+		Object exec = GlobalEventExecutor.INSTANCE;
+		System.out.println(exec);
+
+		try {
+			Log4jUtils.init();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		BeanUtils.init();
+	}
 
 	@Override
 	protected UserService getUserService() {
@@ -62,7 +80,24 @@ public class Client extends AbstractClient {
 		return super.listUser();
 	}
 
-	public static void main(String[] args) throws RunnerException {
+	public static void main(String[] args) throws Exception {
+		Client client = new Client();
+
+		for (int i = 0; i < 60; i++) {
+			try {
+				System.out.println(client.existUser());
+				System.out.println(client.createUser());
+				System.out.println(client.getUser());
+				System.out.println(client.listUser());
+				break;
+			} catch (Exception e) {
+				e.printStackTrace();
+				Thread.sleep(1000);
+			}
+		}
+		
+		System.exit(1);
+
 		Options opt = new OptionsBuilder()//
 				.include(Client.class.getSimpleName())//
 				.warmupIterations(3)//
--- servicecomb-server/src/main/java/benchmark/rpc/Server.java
+++ servicecomb-server/src/main/java/benchmark/rpc/Server.java
@@ -3,9 +3,14 @@ package benchmark.rpc;
 import org.apache.servicecomb.foundation.common.utils.BeanUtils;
 import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
 
+import io.netty.util.concurrent.GlobalEventExecutor;
+
 public class Server {
 
 	public static void main(String[] args) throws Exception {
+		Object exec = GlobalEventExecutor.INSTANCE;
+		System.out.println(exec);
+
 		Log4jUtils.init();
 		BeanUtils.init();
 	}
