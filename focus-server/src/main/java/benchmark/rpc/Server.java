1cbc1d5a5486968e6a75440942da2dc327750e91
dinstone <dinstone@163.com>
2022-11-10 15:16:00 +0800

    add focus rpc test

---
+++ focus-server/src/main/java/benchmark/rpc/Server.java
@@ -0,0 +1,29 @@
+package benchmark.rpc;
+
+import benchmark.service.UserService;
+import benchmark.service.UserServiceServerImpl;
+import com.dinstone.focus.server.FocusServer;
+import com.dinstone.focus.server.ServerOptions;
+
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        ServerOptions serverOptions = new ServerOptions().listen("localhost", 3333)
+                .setEndpoint("focus.server");
+        FocusServer server = new FocusServer(serverOptions);
+
+        // exporting service
+        server.exporting(UserService.class, new UserServiceServerImpl());
+
+        try {
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        server.destroy();
+
+    }
+
+}
